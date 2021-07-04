07defdff5aeaac7e68bcd86a90df695b7368be86
李 翼宏 <870684980@qq.com>
2021-07-04 22:41:01 +0800

    httpclient请求

---
+++ 02NIO/demo/src/main/java/com/nio/demo/httpclient/HtppServer.java
@@ -0,0 +1,34 @@
+package com.nio.demo.httpclient;
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class HtppServer {
+
+    public static void main(String[] args) {
+        doGet("http://localhost:8802/");
+    }
+
+    public static String doGet(String url) {
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+            HttpResponse response = client.execute(request);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String strResult = EntityUtils.toString(response.getEntity());
+                return strResult;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+}
